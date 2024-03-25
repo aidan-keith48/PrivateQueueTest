74c410507f9b682b8b24cf08ee965a433273c0d8
Aidan Naidoo <ST10109482@vcconnect.edu.za>
2024-03-25 07:56:46 +0200

    Add project files.

---
+++ PrivateQueueTest/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PrivateQueueTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageTxt = new System.Windows.Forms.TextBox();
+            this.getMessageTxt = new System.Windows.Forms.TextBox();
+            this.startCalcBtn = new System.Windows.Forms.Button();
+            this.SendQueueDataBtn = new System.Windows.Forms.Button();
+            this.myBrowserBtn = new System.Windows.Forms.Button();
+            this.readMsgBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageTxt
+            // 
+            this.messageTxt.BackColor = System.Drawing.Color.Silver;
+            this.messageTxt.Location = new System.Drawing.Point(24, 25);
+            this.messageTxt.Multiline = true;
+            this.messageTxt.Name = "messageTxt";
+            this.messageTxt.Size = new System.Drawing.Size(1107, 142);
+            this.messageTxt.TabIndex = 0;
+            // 
+            // getMessageTxt
+            // 
+            this.getMessageTxt.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.getMessageTxt.Location = new System.Drawing.Point(24, 189);
+            this.getMessageTxt.Multiline = true;
+            this.getMessageTxt.Name = "getMessageTxt";
+            this.getMessageTxt.ReadOnly = true;
+            this.getMessageTxt.Size = new System.Drawing.Size(1107, 142);
+            this.getMessageTxt.TabIndex = 1;
+            // 
+            // startCalcBtn
+            // 
+            this.startCalcBtn.Location = new System.Drawing.Point(260, 399);
+            this.startCalcBtn.Name = "startCalcBtn";
+            this.startCalcBtn.Size = new System.Drawing.Size(201, 88);
+            this.startCalcBtn.TabIndex = 4;
+            this.startCalcBtn.Text = "Start Calc";
+            this.startCalcBtn.UseVisualStyleBackColor = true;
+            // 
+            // SendQueueDataBtn
+            // 
+            this.SendQueueDataBtn.Location = new System.Drawing.Point(24, 399);
+            this.SendQueueDataBtn.Name = "SendQueueDataBtn";
+            this.SendQueueDataBtn.Size = new System.Drawing.Size(201, 88);
+            this.SendQueueDataBtn.TabIndex = 5;
+            this.SendQueueDataBtn.Text = "Send Queue Data";
+            this.SendQueueDataBtn.UseVisualStyleBackColor = true;
+            this.SendQueueDataBtn.Click += new System.EventHandler(this.SendQueueDataBtn_Click);
+            // 
+            // myBrowserBtn
+            // 
+            this.myBrowserBtn.Location = new System.Drawing.Point(498, 399);
+            this.myBrowserBtn.Name = "myBrowserBtn";
+            this.myBrowserBtn.Size = new System.Drawing.Size(201, 88);
+            this.myBrowserBtn.TabIndex = 6;
+            this.myBrowserBtn.Text = "My Browser";
+            this.myBrowserBtn.UseVisualStyleBackColor = true;
+            // 
+            // readMsgBtn
+            // 
+            this.readMsgBtn.Location = new System.Drawing.Point(930, 409);
+            this.readMsgBtn.Name = "readMsgBtn";
+            this.readMsgBtn.Size = new System.Drawing.Size(201, 88);
+            this.readMsgBtn.TabIndex = 7;
+            this.readMsgBtn.Text = "Read Messages";
+            this.readMsgBtn.UseVisualStyleBackColor = true;
+            this.readMsgBtn.Click += new System.EventHandler(this.readMsgBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 533);
+            this.Controls.Add(this.readMsgBtn);
+            this.Controls.Add(this.myBrowserBtn);
+            this.Controls.Add(this.SendQueueDataBtn);
+            this.Controls.Add(this.startCalcBtn);
+            this.Controls.Add(this.getMessageTxt);
+            this.Controls.Add(this.messageTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messageTxt;
+        private System.Windows.Forms.TextBox getMessageTxt;
+        private System.Windows.Forms.Button startCalcBtn;
+        private System.Windows.Forms.Button SendQueueDataBtn;
+        private System.Windows.Forms.Button myBrowserBtn;
+        private System.Windows.Forms.Button readMsgBtn;
+    }
+}
+
---
+++ PrivateQueueTest/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Messaging;
+using static J3QQ4.Emoji;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace PrivateQueueTest
+{
+    public partial class Form1 : Form
+    {
+        private MessageQueue myMessageQueue = new MessageQueue();
+
+        int UserID = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            this.myMessageQueue.Path = @".\private$\NewQueue" + this.UserID;
+            this.myMessageQueue.Formatter = new XmlMessageFormatter(
+            new Type[] { typeof(string) });
+        }
+
+        private void SendQueueDataBtn_Click(object sender, EventArgs e)
+        {
+            if (MessageQueue.Exists(this.myMessageQueue.Path))
+            {
+                this.SendData2Queue();
+            }
+            else
+            {
+                MessageQueue.Create(this.myMessageQueue.Path);
+                this.SendData2Queue();
+            }
+        }
+
+        //----------------------------------------------------------------------------------
+
+        public void SendData2Queue()
+        {
+            try
+            {
+                this.myMessageQueue.Send(this.messageTxt.Text.Trim() + J3QQ4.Emoji.Alien + "\r\n" +
+                    DateTime.Now.ToString());
+                this.myMessageQueue.Close();
+                this.Text = "Message Count:" + this.Count(this.myMessageQueue);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                this.myMessageQueue.Close();
+            }
+        }
+
+        //----------------------------------------------------------------------------------
+
+        public int Count(MessageQueue queue)
+        {
+            int count = 0;
+            var emunerator = queue.GetMessageEnumerator();
+
+            while (emunerator.MoveNext())
+            {
+                count++;
+            }
+
+            this.Text = "Total Messages in Queue" + count;
+            return count;
+        }
+
+        private void readMsgBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageQueue.Exists(this.myMessageQueue.Path))
+                {
+                    if (this.Count(this.myMessageQueue) > 0)
+                    {
+                        var myMessage = this.myMessageQueue.Receive();
+                        this.getMessageTxt.Text = myMessage.Body.ToString();
+                        return;
+                    }
+                }
+                this.Count(this.myMessageQueue);
+                MessageBox.Show("No Message");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //----------------------------------------------------------------------------------
+
+    }
+}
